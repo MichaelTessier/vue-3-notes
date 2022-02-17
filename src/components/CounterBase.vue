435f05d324ee09705ec43034920171c5d991ac6e
Michael Tessier <michael.tessier.paris@gmail.com>
2022-02-17 17:52:27 +0100

    feat(app): add simple couter component

---
+++ src/components/CounterBase.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    CounterBase
+
+    <p>Message: {{ message }}</p>
+
+    <p>
+      Count: {{ count }}
+
+      <button @click="increment">Increment</button>
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed, defineProps, ref } from "vue";
+
+interface Props {
+  message: string;
+  limit?: number;
+}
+
+const props = defineProps<Props>();
+
+const count = ref<number>(0);
+
+const canIncrement = computed((): boolean => {
+  return !!props.limit && count.value < props.limit;
+});
+
+const increment = (): void => {
+  if (!canIncrement.value) return;
+  count.value++;
+};
+</script>
+
+<style scoped></style>
