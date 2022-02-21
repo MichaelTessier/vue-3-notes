8ec4adec3b073b79157d30531b5630dbfce76b7d
Michael Tessier <michael.tessier.paris@gmail.com>
2022-02-21 04:15:57 +0100

    config(firebase): init

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { firebaseApp } from "@/config/firebase";
+firebaseApp();
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
