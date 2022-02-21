206bad478138809be402fba1f16142c1e7072378
Michael Tessier <michael.tessier.paris@gmail.com>
2022-02-21 05:04:05 +0100

    feat(auth): add sign-up view

---
+++ src/domains/auth/views/SignUp.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h1>Sign UP</h1>
+    <form @submit.prevent="submit">
+      <input
+        type="email"
+        name="email"
+        id="email"
+        :placeholder="'Email'"
+        v-model="email"
+      />
+      <input
+        type="password"
+        name="password"
+        id="password"
+        :placeholder="'Password'"
+        v-model="password"
+      />
+      <button type="submit">Register</button>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "vue-router";
+
+const email = ref("");
+const password = ref("");
+
+const router = useRouter();
+const auth = getAuth();
+
+const submit = async (): Promise<void> => {
+  try {
+    await createUserWithEmailAndPassword(auth, email.value, password.value);
+
+    router.push({
+      name: "home",
+    });
+  } catch (error) {
+    alert(error);
+  }
+};
+</script>
+
+<style scoped></style>
